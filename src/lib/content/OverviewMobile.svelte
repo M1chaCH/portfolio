3f5d24f3beba30e0d54faa9dd516789ef2ef9abe
Micha Schweizer <micha_ch@outlook.com>
2023-02-01 15:38:56 +0100

    implement overview and menu (#2)

---
+++ src/lib/content/OverviewMobile.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import {currentLanguage, username} from "../stores";
+
+    $: t_welcome = $currentLanguage["overview"]["welcome"];
+    $: t_description = $currentLanguage["overview"]["description"];
+</script>
+
+<section id="overview">
+    <div class="text-container">
+        <h2>{t_welcome} {$username}</h2>
+        <p>{t_description}</p>
+    </div>
+    <img src="assets/images/welcome-image.jpg" alt="welcome-image" />
+</section>
+
+<style lang="scss">
+  #overview {
+    height: 100vh;
+    width: 100vw;
+
+    box-sizing: border-box;
+    position: relative;
+
+    .text-container {
+      margin: 0 30px;
+      position: absolute;
+      bottom: 10px;
+
+      h2 {
+        text-transform: uppercase;
+      }
+    }
+
+    img {
+      border-radius: 50px;
+      margin-left: 30px;
+
+      position: absolute;
+      top: 20vh;
+      left: -100px;
+
+      width: 90vw;
+      height: 45vh;
+      object-fit: cover;
+    }
+  }
+</style>
--- src/lib/language-selector/languages.ts
+++ src/lib/language-selector/languages.ts
@@ -3,10 +3,22 @@ export const en = {
     "contact": "Contact Me",
     "contactSubject": "Contact request",
     "hello": {
-        "welcome": "Welcome",
+        "welcome": "Hi 🙋‍♂️",
         "namePlaceholder": "Your name",
         "nameReason": "Enter your name if you want to be addressed directly. (name is never stored)",
         "continue": "Continue",
+    },
+    "overview": {
+        "welcome": "Hi",
+        "description": "With the next few scrolls I am going to guide you through my life.",
+    },
+    "menu": {
+        "welcome": "Welcome",
+        "overview": "Overview",
+        "career": "Career",
+        "hobbies": "Hobbies",
+        "projects": "Projects",
+        "skills": "Skills",
     },
 }
 
@@ -15,10 +27,22 @@ export const de = {
     "contact": "Kontaktiere mich",
     "contactSubject": "Kontaktanfrage",
     "hello": {
-        "welcome": "Willkommen",
+        "welcome": "Hallo 🙋‍♂️",
         "namePlaceholder": "Dein Name",
         "nameReason": "Gib dein Name ein damit ich dich direkt ansprechen kann. (wird nicht gespeichert)",
         "continue": "Weiter",
+    },
+    "overview": {
+        "welcome": "Hallo",
+        "description": "Mit den nächsten paar scrolls werde ich dich durch mein Leben führen.",
+    },
+    "menu": {
+        "welcome": "Willkommen",
+        "overview": "Übersicht",
+        "career": "Kariere",
+        "hobbies": "Hobbies",
+        "projects": "Projekte",
+        "skills": "Skills",
     },
 }
 
